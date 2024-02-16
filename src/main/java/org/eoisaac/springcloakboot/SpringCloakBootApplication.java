b0637db89ee75b2358c12f20e9230d035accf96c
Isaac Santiago <isaac.pereira@zumpy.com.br>
2024-02-16 09:19:43 -0300

    feat: create spring-boot project

---
+++ src/main/java/org/eoisaac/springcloakboot/SpringCloakBootApplication.java
@@ -0,0 +1,13 @@
+package org.eoisaac.springcloakboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloakBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloakBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/eoisaac/springcloakboot/SpringCloakBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.eoisaac.springcloakboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloakBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
